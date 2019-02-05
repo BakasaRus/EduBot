432972af90ccd768c82c8b828321196363189b16
Nick Andreev <bakasaru@list.ru>
2019-02-05 23:15:05 +0300

    Update Subscribers table to make it less complicated

--- resources/views/subscribers/index.blade.php
+++ resources/views/subscribers/index.blade.php
@@ -12,23 +12,30 @@
                 <tr>
                     <th>ID</th>
                     <th>ФИО</th>
-                    <th>Создан</th>
-                    <th>Изменён</th>
-                    <th>Удалён</th>
+                    <th>Согласие на рассылку</th>
                 </tr>
                 </thead>
                 <tbody>
                 @foreach($subscribers as $subscriber)
                     <tr>
-                        <td>{{ $subscriber->id }}</td>
+                        <td>
+                            <a href="https://vk.com/id{{ $subscriber->id }}" target="_blank">
+                                {{ $subscriber->id }}
+                            </a>
+                        </td>
                         <td>
                             <a href="{{ route('subscribers.show', ['id' => $subscriber->id]) }}">
                                 {{ $subscriber->full_name }}
                             </a>
                         </td>
-                        <td>{{ $subscriber->created_at }}</td>
-                        <td>{{ $subscriber->updated_at }}</td>
-                        <td>{{ $subscriber->deleted_at }}</td>
+                        <td>
+                            {{-- Тут есть смайлики, которые могут не отображаться некоторыми шрифтами --}}
+                            @if($subscriber->deleted_at)
+                                ❌ Нет
+                            @else
+                                ✔️ Да
+                            @endif
+                        </td>
                     </tr>
                 @endforeach
                 </tbody>
